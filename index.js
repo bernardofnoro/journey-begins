88f9d0afd8fe9bd8167b688ac3f48344673c7169
bernardofnoro <89053829+bernardofnoro@users.noreply.github.com>
2021-08-18 23:59:41 +0000

    Pratica blue

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var nome =prompt(`What is your name?`);
 alert(`Hello, `+ nome +` welcome to the programming world!`);
+prompt
+var endereco =prompt(`Where are you from?`);
+alert(`Wow! `+ endereco +` must be beautiful! We are just a few questions away!`);
+
+var obj =prompt(`What is your main goal with us?`);
+console.log(`Thank you for all the informations!`);
+
 
 
